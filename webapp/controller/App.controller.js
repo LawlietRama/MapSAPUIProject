25b5c049dc0acd6d5dae7584513044d0ada63517
LawlietRama <auliarama3499@gmail.com>
2022-06-07 11:57:20 +0700

    following tutorial to make another page

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,4 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-  ],
-  function (Controller, MessageToast, JSONModel) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onInit: function () {
-        var oData = {
-          recipient: {
-            name: "UI5",
-          },
-        };
-        var oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-      },
-      onShowHello: function () {
-        //Show native or vanilla JS alert
-        MessageToast.show("Si paling halo");
-      },
-    });
-  }
-);
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
+});
